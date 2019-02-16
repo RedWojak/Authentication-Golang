1e407fdc3e9d56e315d98ad8f86fe24fd1d72822
redwojak@gmail.com <redwojak@gmail.com>
2019-02-16 13:30:49 +0300

    JWT client init

--- main.go
+++ main.go
@@ -1,8 +1,46 @@
 package main
-import "fmt"
-func main() {
-    fmt.Println("hello world")
 
-//test 1
+import (
+	"fmt"
+	//"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+var mySigningKey = []byte("password")
+
+func GenerateJWT() (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+    /*
+	claims["user_id"] = "6e66b20c-4599-41f7-9665-0bbab5ce4e3e" //UUID
+	claims["exp"] = time.Now().Add(time.Second * 3600).Unix()
+	claims["orig_iat"] = time.Now().Unix()
+    */
+    claims["orig_iat"] = 1550312698
+    claims["user_id"] = "6e66b20c-4599-41f7-9665-0bbab5ce4e3e" //UUID
+	claims["exp"] = 1550313598
+	
+
+
+
+	tokenString, err := token.SignedString(mySigningKey)
+
+	if err != nil {
+		fmt.Errorf("Something went wrong: %s", err.Error())
+		return "", err
+	}
+
+	return tokenString, nil
+
+}
+
+func main() {
+	fmt.Println("JWT test Client")
+	tokenString, err := GenerateJWT()
+	if err != nil {
+		fmt.Println("Error generating token")
+	}
+	fmt.Println(tokenString)
 
 }
